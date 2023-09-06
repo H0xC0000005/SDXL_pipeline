0d8db047e40e4efa60dde49b93e0fb5ef5ff366a
H0xC0000005 <zh0001hu@e.ntu.edu.sg>
2023-09-06 14:38:17 +0800

    push back

--- bash_maker.py
+++ bash_maker.py
@@ -1,8 +1,8 @@
 import time
 
 def make_job_bash(job_name: str, gpu_id_list: list[int]):
-    with open(f"{job_name}_{gpu_id_list}.sh", "+w"): 
+    with open(f"{job_name}_{gpu_id_list}.sh", "w") as file:
         for gpu_id in gpu_id_list:
-        
+            file.write(f"python3 {job_name}.py ")
     
 
